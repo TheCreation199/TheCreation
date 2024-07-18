3c76d624f888e10f3a03af46c51367e5339f6441
TheCreation199 <thecreationteams@gmail.com>
2024-07-18 19:31:07 +0530

    x9

--- js/databse.js
+++ js/databse.js
@@ -5,7 +5,7 @@ document.getElementById('myForm').addEventListener('submit', function(event) {
     let formData = new FormData(this);
     
     // Send formData to Google Apps Script function for processing
-    fetch('https://script.google.com/macros/s/your-script-id/exec', {
+    fetch('https://script.google.com/macros/s/AKfycbxvBhuxbaBh7pfGgLinOSRG1sdgOw3mD_1wXWKIgz8kP9uF9d1doEJTogiOWk4vBRAGYQ/exec', {
         method: 'POST',
         body: formData
     })
