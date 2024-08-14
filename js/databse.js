eea26c66f3215dbf3248a2027260baf5ade335d7
TheCreation199 <thecreationteams@gmail.com>
2024-08-14 18:01:50 +0530

    x15

--- js/databse.js
+++ js/databse.js
@@ -1,22 +1,76 @@
-document.getElementById('myForm').addEventListener('submit', function(event) {
+const form = document.getElementById('myForm')
+
+
+var options = {
+    "async": true,
+    "crossDomain": true,
+    "method": 'GET',
+    "headers": {
+        'Content-Type': 'text/plain;charset=utf-8'
+
+    },
+    "redirect": 'follow',
+
+};
+
+
+form.addEventListener("submit", function(event) {
     event.preventDefault();
-    
-    // Get form data
-    let formData = new FormData(this);
-    
-    // Send formData to Google Apps Script function for processing
-    fetch('https://script.google.com/macros/s/AKfycbwNT9DY9lCoiVwhIp4TA94ewIbRBGOs5Xnmql9vQySd8XIZVxSXEr0f8h6UcV2U0wv1/exec', {
-        method: 'POST',
-        body: formData
-    })
-    .then(response => response.json())
-    .then(data => {
-        console.log('Success:', data);
-        // Optionally, display a success message to the user
-    })
-    .catch((error) => {
-        console.log('Error:', error);
-        // Handle errors, e.g., display an error message to the user
-    });
-});
-console.log("x16")
+
+    var formdata = new FormData(form);
+    var params = new URLSearchParams(formdata).toString()
+    const url = 'https://script.google.com/macros/s/AKfycbwNT9DY9lCoiVwhIp4TA94ewIbRBGOs5Xnmql9vQySd8XIZVxSXEr0f8h6UcV2U0wv1/exec' + '?'+ params
+console.log(url)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    fetch(url, options)
+        .then(response => response.json())
+
+
+
+        .then(data =>{
+console.log("success", data)
+
+
+
+
+        })
+
+
+
+
+
+
+
+
+
+
+
+        .catch((error) => {
+
+
+            console.log("error", error)
+
+
+        })
+
+
+})
